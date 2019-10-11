b1c3395414de2e9d054967cd3b5ada22efa02452
Eronbello <eron.bello@gmail.com>
2019-10-10 21:01:48 -0300

    add sucrase and nodemon

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express'
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ hello: 'World' });
+  return res.json({ hello: 'teste' });
 })
 
-module.exports = routes;
+export default routes;
