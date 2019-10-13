3752708b72543cc28ad39185f8ab0a0257430561
Eronbello <eron.bello@gmail.com>
2019-10-13 17:43:58 -0300

    add sequelize

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '33461486',
+  database: 'gobarber',
+  define: {
+    timestaps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
